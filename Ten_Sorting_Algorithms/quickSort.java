85d670e289cfd7fd73c7dcd4c9503c8f82d39b09
ZhengqiY <51040140+ZhengqiY@users.noreply.github.com>
2020-03-22 21:28:58 -0700

    Create quickSort.java

---
+++ Ten_Sorting_Algorithms/quickSort.java
@@ -0,0 +1,26 @@
+    public int partition(int[] a, int low, int high){
+        int left = low;
+        int right = high;
+        int pivot = a(low);
+
+        while(true){
+            while(left<high && a(left)<=pivot){
+                left++;
+            }
+            while(right>low && a(right)>=pivot){
+                right--;
+            }
+            if (left>=right) break;
+            swap(left,right);
+        }
+        swap(low,right);
+        return right;
+    }
+    
+    public void quickSort(int[] a, int low, int high){
+        if (low<high){
+            int s = partition(a, low, high);
+            quickSort(a, low, s-1);
+            quickSort(a, s+1, high);
+        }
+    }
