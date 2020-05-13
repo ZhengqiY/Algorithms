f9ebe8b4ff46fe2b6c2d494c55c60511942da78f
ZhengqiY <51040140+ZhengqiY@users.noreply.github.com>
2020-05-13 16:56:42 -0700

    2D DP

---
+++ Dynamic Programming/UniquePath.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class UniquePath {
+public:
+	int uniquePath(int m, int n)
+	{
+		vector<vector<int>> dp(m, vector<int>(n, 0));
+		dp[0][0] = 1;
+		for (int i = 1; i <= n - 1; i++) {
+			dp[0][i] = 1;
+		}
+		for (int i = 1; i <= m - 1; i++) {
+			dp[i][0] = 1;
+		}
+		for (int row = 1; row <= m - 1; row++) {
+			for (int col = 1; col <= n - 1; col++) {
+				dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+};
